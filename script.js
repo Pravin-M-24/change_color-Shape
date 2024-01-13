3398a982811a4b2f6b198cd002a9bb460801ccfe
Your Name <you@example.com>
2024-01-13 22:13:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let color =[
+    "red",
+    "cyan",
+    "black",
+    "voilet",
+    "blue",
+    "yellow",
+    "purple",
+    "orange",
+    "lightgreen",
+    "lemon"
+];
+let classLst = [
+    "triangle","traiangle-up","triangle-down","rectangle","circle_inner","trapezoid",
+]
+// let cg =  document.getElementById("circle_bg");
+  function changeColor(){
+     let cg =  document.getElementById("circle_bg");
+    let index = Math.floor(Math.random() * color.length); 
+         console.log(index);
+        cg.style.backgroundColor = color[index];
+  }
+  function changeShape(){
+    let div = document.getElementById("sp");
+
+   let index = Math.floor(Math.random() * classLst.length);
+    while(div.className != classLst[index]) div.className = classLst[index];
+}
+  
+
+ let color_btn=  document.getElementById("changeColor");
+ let shape_btn = document.getElementById("changeShape");
+
+   color_btn.addEventListener("click" , changeColor);
+    shape_btn.addEventListener("click",changeShape);
